10ca0959d07d0d98d457b6c77b947160dc2546b3
Cedric-X02 <81324258+Cedric-X02@users.noreply.github.com>
2021-09-25 22:19:04 +0200

    Add files via upload

---
+++ lib/smarty_plugins/block.tr.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Smarty plugin
+ * @package OpenUpload
+ * @subpackage plugins
+ */
+
+
+/**
+ * Smarty {tr} block plugin
+ *
+ * Type:     block
+ * Name:     tr
+ * Purpose:  Translate contained text with a user defined function "translate"
+ * @author Alessandro Briosi 
+ * @param array
+ * @param Smarty
+ * @return string
+ * @uses translate()
+ */
+
+function smarty_block_tr($params, $content = null, &$smarty, &$repeat) {
+
+  if(!$repeat){
+    if (isset($content)) {
+      return translate($content, 'template', $params);
+    } else {
+      return '';
+    }
+  }
+}
+
+?>
